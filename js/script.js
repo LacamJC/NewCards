8524939d89d2960e06c3cc52d258fc4252a8f73a
LacamJC <joaoramajo744@gmail.com>
2024-02-16 23:47:15 -0300

    update script.js to receive data from API

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
 const container = document.getElementById('container');
+// const apiUrl = 'http://localhost:3000/api'; // Endereço da api 
+const Status = document.getElementById('status');
 
-const data = [
+
+
+
+
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(apiData =>{
+    console.log(apiData);
+    const data = apiData;
+    Status.innerHTML = "Sim"
+
+    for(let i =0; i < data.length; i++)
     {
-<<<<<<< HEAD
+        var template=`
+        <div class="card" id="${data[i].id}">
+            <div class="card__header">
+                <img src="${data[i].url_img}" class="card__header--img" alt="${data[i].alt_txt}">
+            </div>
+            
+            <div class="card__body">
+                <h1 class="card__body--title">${data[i].site_name}</h1>
+                <p class="card__body--text">${data[i].body_text}</p>
+            </div>
+            <div class="card__footer">
+                <a href="${data[i].a_link}" target="_blank" title="Ir para ${data[i].site_name}" class="card__footer--a"><div class="card__footer--btn">
+                Visitar 
+                <div class="line"></div>
+                </div></a>
+            </div> `
+    
+            container.innerHTML += template
+    }
+}).catch(error =>{
+    console.log(`Erro ao buscar dados da api: ${error}`)
+    Status.innerHTML = "Não"
+    const data = [
+    {
         id : "facebook",
         url_img : "/img/facebook_logo.svg",
-=======
-        url_img : "img/facebook_logo.svg",
->>>>>>> a6dc357cb98d60671c871c8566132a937660c4df
         alt_txt : "Icone do facebook",
         body_text : "O Facebook é uma rede social que permite conversar com amigos e compartilhar mensagens, links, vídeos e fotografias",
         a_link : "https://www.facebook.com/",
@@ -85,6 +119,8 @@ for(let i =0; i < data.length; i++)
         container.innerHTML += template
 }
 
+
+});
 
 
 
